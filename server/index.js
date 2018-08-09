e58e9f18ba3e20b788ecb7198330fa60c4f4f373
Rui.Liu@Dimensional.com <Rui.Liu@dimensional.com>
2018-08-08 19:29:03 -0500

    init commit

--- server/index.js
+++ server/index.js
@@ -4,3 +4,33 @@ let products = require("./products");
 let vehicles = require("./vehicles");
 let contacts = require("./contacts");
 
+const app = express();
+
+//tell the sever to GET the file
+//find the static page in PUBLIC FOLDER
+//app.use(express.static('public'));
+
+app.get("/comments",function(request,response){
+    response.json(comments);
+});
+app.get("/products",function(request,response){
+  response.json(products);
+});
+app.get("/vehicles",function(request,response){
+  response.json(vehicles);
+});
+app.get("/contacts",function(request,response){
+    response.json(contacts);
+});
+
+//req->request, res->response
+
+
+app.listen(3001, (err) => {
+if (err) {
+  return console.log("Error", err);
+}
+console.log("Web server is now living in apartment 3001");
+});
+
+
